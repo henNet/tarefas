f6079f718200a30bd0456508c01c4aa5fafcfd9a
henNet <dmf.n123@gmail.com>
2024-05-05 01:30:45 -0300

    Criado componente Task e consumido API do back com as tarefas

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,21 +1,50 @@
 import "./home.css";
+import Task from "../../components/task/task";
+import { useEffect, useState } from "react";
 
 function Home() {
-  return (
-    <div className="container-task">
-      <h1>Minhas Taferas</h1>
+  const [allTasks, setAllTasks] = useState([]);
 
-      <div className="form-task">
-        <input
-          className="input-task"
-          type="text"
-          placeholder="Digite uma tarefa ..."
-        />
-        <button className="btn-task">
-            Inserir Tarefa
-        </button>
+  useEffect(() => {
+    fetch("http://localhost:3001/tarefas")
+      .then(function (response) {
+        response.json().then((json) => {
+          setAllTasks(json);
+        });
+      })
+
+      .catch((erro) => {
+        console.log(erro);
+      });
+  });
+
+  // useEffect(()=>{
+  //   setAllTasks(apiFake);
+  //   console.log("Aqui");
+  // });
+
+  return (
+    <>
+      <div className="container-task">
+        <h1>Minhas Taferas</h1>
+
+        <div className="form-task">
+          <input
+            className="input-task"
+            type="text"
+            placeholder="Digite uma tarefa ..."
+          />
+          <button className="btn-task">Inserir Tarefa</button>
+        </div>
+
+        <div>
+          {allTasks.map((task) => {
+            return <Task description={task.descricao} />;
+          })}
+        </div>
       </div>
-    </div>
+      <div className="footer">Feit com ❤️ por Henrique</div>
+    </>
   );
 }
 
